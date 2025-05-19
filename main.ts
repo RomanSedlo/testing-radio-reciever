8e6d779d47cf3b69f3ca04b29aa57f3fdbf09e8c
Roman Sedlář <roman.sedlar.024@pslib.cz>
2025-05-19 21:10:13 +0200

    debir

--- main.ts
+++ main.ts
@@ -10,17 +10,13 @@ type drivingSignal = {
 }
 
 let serialNumber: number = 908640693;
-let keySqequence: Array<number> = [4, 8, 6, 1, 5, 3, 4, 7, 2, 6, 0, 3, 9, 4, 8, 2, 6, 3, 7, 5, 2, 1, 0, 6, 8, 3, 5, 9];
 
 let newDrivingPackage: drivingSignal
 
 radio.onReceivedString(function (drivingPackage: string) {
+    basic.showString(drivingPackage)
     if (radio.receivedPacket(RadioPacketProperty.SerialNumber) === serialNumber) {
-        basic.showString("W")
         newDrivingPackage = JSON.parse(drivingPackage) as drivingSignal
-        if (newDrivingPackage.k === keySqequence[1]) {
-            
-
-        }
+        basic.showString("W")
     }
 })
