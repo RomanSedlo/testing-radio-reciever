421aefd20f87973a635b1e552738acbe098bad6e
Roman Sedlář <roman.sedlar.024@pslib.cz>
2025-05-19 20:38:07 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,22 +4,24 @@ radio.setFrequencyBand(52)
 radio.setTransmitSerialNumber(true)
 
 type drivingSignal = {
+    k: number;
     x: number;
     y: number;
     z: number
 }
 
+let serialNumber = 1234;
+let keySqequence: Array<number> = [4, 8, 6, 1, 5, 3, 4, 7, 2, 6, 0, 3, 9, 4, 8, 2, 6, 3, 7, 5, 2, 1, 0, 6, 8, 3, 5, 9];
+
 let newDrivingPackage: drivingSignal
-let unlock = false
 
 
 radio.onReceivedString(function (drivingPackage: string) {
-    let serialNumber = radio.receivedPacket(RadioPacketProperty.SerialNumber)
     if (serialNumber === radio.receivedPacket(RadioPacketProperty.SerialNumber)) {
         newDrivingPackage = JSON.parse(drivingPackage) as drivingSignal
         //zde bude kód na ovládání autíčka
-        if (newDrivingPackage.x === 1) {
-            basic.showString("W")
-        }
+        if (newDrivingPackage.k === keySqequence[1]) {
+            basic.showString("K")
+        } else basic.showString("W")
     }
 })
