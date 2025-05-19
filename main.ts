cc715f85c9bc5926a85136dd2327efdfe0677dc9
Roman Sedlář <roman.sedlar.024@pslib.cz>
2025-05-19 22:18:37 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -14,13 +14,12 @@ let serialNumber = -1584843917;
 let recievedPackage: string
 let drivingPackage: drivingSignal = { x: 0, y: 0, z: 0 }
 
-
 radio.onReceivedString(function (recievedPackage: string) {
     if (radio.receivedPacket(RadioPacketProperty.SerialNumber) === serialNumber) {
         let hyperPackage = recievedPackage.split(",")
         drivingPackage.x = parseInt(hyperPackage[0])
         drivingPackage.y = parseInt(hyperPackage[1])
         drivingPackage.z = parseInt(hyperPackage[2])
-        basic.showString("W")
     }
 })
+
